1dcafe2e5351c03b9075bedf02bbb44ac88eebe4
Okal Otieno <okal@justokal.com>
2016-03-16 10:38:37 +0300

    OO: Move progress bar in demo

--- src/songa/core.cljs
+++ src/songa/core.cljs
@@ -10,23 +10,36 @@
 (def progress-atom
   (atom {:percentage-completed 10}))
 
+(defn start-moving
+  []
+  (let [incrementer (fn [] (swap! progress-atom
+                                  (fn [{:keys [percentage-completed]}]
+                                    {:percentage-completed
+                                     (if (< percentage-completed 100)
+                                       (inc percentage-completed)
+                                       0)})))]
+    (js/setInterval incrementer 200)))
+
 (defcard first-card
   (sab/html [:div
              [:h1 "This is your first devcard!"]]))
 
 (defn progress-bar
-  [{:keys [percentage-completed]}]
-  (om/component
-   (sab/html
-    [:div {:class "progress-bar"}
-     [:div {:class "percentage-indicator"
-            :style {:width (str percentage-completed "%")}}]])))
+  [{:keys [percentage-completed]} owner]
+  (reify
+    om/IRender
+    (render [_]
+      (sab/html
+       [:div {:class "progress-bar"}
+        [:div {:class "percentage-indicator"
+               :style {:width (str percentage-completed "%")}}]]))))
 
 (defcard progress-bar-card
   (om-root progress-bar)
   progress-atom)
 
 (defn main []
+  (start-moving)
   ;; conditionally start the app based on whether the #main-app-area
   ;; node is on the page
   (when-let [node (.getElementById js/document "main-app-area")]
